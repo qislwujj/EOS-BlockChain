d95fedb02394495ec48a682211c04ec169222aad
root <root@localhost.localdomain>
2019-07-17 13:49:08 +0900

    contracts[check(has_auth2)]

--- contrects/helloworld/helloworld.cpp
+++ contrects/helloworld/helloworld.cpp
@@ -10,9 +10,9 @@ CONTRACT helloworld : public contract {
     // 사용 코드
     // ./cleos.sh push action 컨트랙트계정 hi '["파라미터계정"]' -p 실행주체계정(권한있는계정)
     ACTION hi(name user){
-        require_auth(user);
+        // require_auth(user);
         //계정 검사 코드
-            check(has_auth(user),"XXXXX");
+            check(has_auth(user),"xxxxx");
         print("hello,",user);
     }
     private:
