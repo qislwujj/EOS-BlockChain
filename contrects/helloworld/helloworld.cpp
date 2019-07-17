f4e47919d11db6983b3d889f2b39b57cc10baae2
root <root@localhost.localdomain>
2019-07-17 13:28:03 +0900

    contract[require]

---
+++ contrects/helloworld/helloworld.cpp
@@ -0,0 +1,19 @@
+#include <eosiolib/eosio.hpp>
+
+using namespace eosio;
+
+CONTRACT helloworld : public contract {
+    public:
+    using contract::contract;
+
+
+    // 사용 코드
+    // ./cleos.sh push action 컨트랙트계정 hi '["파라미터계정"]' -p 실행주체계정(권한있는계정)
+    ACTION hi(name user){
+        require_auth(user);
+        //계정 검사 코드
+
+        print("hello,",user);
+    }
+    private:
+};
