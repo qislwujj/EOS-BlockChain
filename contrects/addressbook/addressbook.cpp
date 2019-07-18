960fa64f3c469753f0912cded2687af9e1bed580
root <root@localhost.localdomain>
2019-07-18 10:55:19 +0900

    multiindex base[insert&erase3]

--- contrects/addressbook/addressbook.cpp
+++ contrects/addressbook/addressbook.cpp
@@ -2,49 +2,51 @@
 
 using namespace eosio;
 
-CONTRACT addressboo: public contract{
-    public:
-    using contract::contract;
-        ACTION upsert(){}
+
+CONTRACT addressbook: public contract {
+public:
+using contract::contract;
 
 
-        ACTION insert(name user, std::string first_name,std::string last_name,uint32_t age){
-            require_auth(user);
-            address_index forInsert(get_self(),get_self().value);
-            auto itr = forInsert.find(user.value);
+ACTION upsert() {}
 
-            check(itr == forInsert.end(),"already exists");
+ACTION insert(name user, std::string first_name, std::string last_name, uint32_t age) {
+require_auth(user);
 
-            forInsert.emplace(user, [&](auto&row){
-                row.user =user;
-                row.first_name = first_name;
-                row.last_name = last_name;
-                row.age = age;
-            });
+address_index forInsert(get_self(), get_self().value);
+auto itr = forInsert.find(user.value);
 
-            print("insert success");
-        }
+check(itr == forInsert.end(), "already exists");
 
+forInsert.emplace(user, [&](auto& row) {
+row.user = user;
+row.first_name = first_name;
+row.last_name = last_name;
+row.age = age;
+});
 
-        ACTION erase(name user) {
-            require_auth(user);
+print("insert success");
+}
 
-            address_index forErase(get_self().get_self.value);
-            auto itr = forErase.require_find(user.value,"no account");
-            forErase.erase(utr)
+ACTION erase(name user) {
+require_auth(user);
 
-            print("erase success");
-        }
-    
-    private:
-    sturct [[eosio::table]] person {
-        name user;
-        std::string first_name;
-        std::string last_name;
-        uint32_t age;
+address_index forErase(get_self(), get_self().value);
+auto itr = forErase.require_find(user.value, "no account");
+forErase.erase(itr);
 
-        uint64_t primary_key() const {return user.value;}
-    };
+print("erase success");
+}
 
-    typedef multi_index<"people"_n,person> address_index;
+private:
+struct [[eosio::table]] person {
+name user;
+std::string first_name;
+std::string last_name;
+uint32_t age;
+
+uint64_t primary_key() const { return user.value; }
+};
+
+typedef multi_index<"people"_n, person> address_index;
 };
