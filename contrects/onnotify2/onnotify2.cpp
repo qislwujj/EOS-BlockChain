f3609fc3fb4916d7d7e6546084578f5017d87517
root <root@localhost.localdomain>
2019-07-23 13:26:08 +0900

    onnotify2

--- contrects/onnotify2/onnotify2.cpp
+++ contrects/onnotify2/onnotify2.cpp
@@ -5,69 +5,44 @@ using namespace eosio;
 
 
 CONTRACT onnotify2: public contract {
+    public:
 
-public:
-
-using contract::contract;
-
-
-ACTION dummy() {}
+    using contract::contract;
 
 
-[[eosio::on_notify("eosio.token::transfer")]]
-
-void ontransfer(name from, name to, asset quantity, std::string memo) {
-
-if(from == get_self()) {
-
-Receive forReceiver(get_self(), get_self().value);
-
-forReceiver.emplace(get_self(), [&](auto& row) {
-
-row.mykey = forReceiver.available_primary_key();
-
-row.user = to;
-
-row.balance = quantity;
-
-});
-
-} else {
-
-Send forSender(get_self(), get_self().value);
-
-forSender.emplace(get_self(), [&](auto& row) {
-
-row.mykey = forSender.available_primary_key();
-
-row.user = from;
-
-row.balance = quantity;
-
-});
-
-}
-
-}
-
-private:
-
-TABLE tradingbook_struct {
-
-uint64_t mykey;
-
-name user;
-
-asset balance;
+    ACTION dummy() {}
 
 
-uint64_t primary_key() const { return mykey; }
+    [[eosio::on_notify("eosio.token::transfer")]]
 
-};
+    void ontransfer(name from, name to, asset quantity, std::string memo) {
 
-
-typedef multi_index<"senderbook"_n, tradingbook_struct> Send;
-
-typedef multi_index<"receiverbook"_n, tradingbook_struct> Receive;
-
+        if(from == get_self()) {
+            Receive forReceiver(get_self(), get_self().value);
+            forReceiver.emplace(get_self(), [&](auto& row) {
+            row.mykey = forReceiver.available_primary_key();
+            row.user = to;
+            row.balance = quantity;
+            });
+        }
+        else {
+            Send forSender(get_self(), get_self().value);
+            forSender.emplace(get_self(), [&](auto& row) {
+            row.mykey = forSender.available_primary_key();
+            row.user = from;
+            row.balance = quantity;
+            });
+        }
+    }
+    
+        private:
+        TABLE tradingbook_struct {
+            uint64_t mykey;
+            name user;
+            asset balance;
+            uint64_t primary_key() const { return mykey; }
+        };
+        
+    typedef multi_index<"senderbook"_n, tradingbook_struct> Send;
+    typedef multi_index<"receiverbook"_n, tradingbook_struct> Receive;
 };
